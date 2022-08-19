0a8931f2ffb4cec76f58fe25bb35f7f4a53b2a4f
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2022-08-19 22:10:49 +0300

    feat: added fetch products by category-id

--- src/pages/category/_id.vue
+++ src/pages/category/_id.vue
@@ -2,18 +2,32 @@
   <section class="applyied">
     <CatalogFilter />
     <div class="categories">
-      <CatalogProducts />
+      <CatalogProducts :products="products" />
     </div>
   </section>
 </template>
 
 <script>
   import CatalogFilter from '~/components/catalog/CatalogFilter.vue'
-  import CatalogProducts from '~/components/catalog/CatalogProducts.vue'
+  import CatalogProducts from '~/components/catalog/CatalogProducts/index.vue'
 
   export default {
     name: 'CategoryIdPage',
     components: { CatalogFilter, CatalogProducts },
     layout: 'catalog',
+
+    async asyncData(ctx) {
+      const id = await ctx.params.id
+      const { data } = await ctx.$axios.$get(`/categories/${id}/products`)
+      return {
+        products: data.products,
+      }
+    },
+
+    data() {
+      return {
+        products: null,
+      }
+    },
   }
 </script>
