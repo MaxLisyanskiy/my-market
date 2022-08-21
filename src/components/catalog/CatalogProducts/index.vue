b8a40af1931fd3d3fb3dbba8c9819b937bf4661e
Maksim Lisianskii <Max_Lisyanskiy@mail.ru>
2022-08-21 22:47:49 +0300

    feat: added viewMode for category products

--- src/components/catalog/CatalogProducts/index.vue
+++ src/components/catalog/CatalogProducts/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <CatalogProductsGallery v-if="productsClass === 'gallery'" :products="products"/>
+    <CatalogProductsGallery v-if="viewMode === 'gallery'" :products="products"/>
     <CatalogProductsList v-else :products="products"/>
   </div>
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+
   import CatalogProductsList from './CatalogProductsList.vue'
-  import CatalogProductsGallery from './CatalogProductsGallery.vue'
+  import CatalogProductsGallery from './CatalogProductsGallery.vue'  
+
   export default {
     name: 'CatalogProducts',
     components: { CatalogProductsList, CatalogProductsGallery },
@@ -17,10 +20,8 @@
         default: () => [],
       },
     },
-    data() {
-      return {
-        productsClass: 'list',
-      }
+    computed: {
+      ...mapState('catalog', ['viewMode']),
     },
 
   }
--- src/components/catalog/CatalogSidebar/index.vue
+++ src/components/catalog/CatalogSidebar/index.vue
@@ -2,16 +2,16 @@
   <aside class="categories-sidebar">
     <span class="categories-sidebar__title">Категории</span>
     <CatalogSidebarMenu />
-    <CatalogSidebarFilter />
+    <!-- <CatalogSidebarFilter /> -->
   </aside>
 </template>
 
 <script>
   import CatalogSidebarMenu from './CatalogSidebarMenu.vue'
-  import CatalogSidebarFilter from './CatalogSidebarFilter.vue'
+  // import CatalogSidebarFilter from './CatalogSidebarFilter.vue'
   export default {
     name: 'CatalogSidebar',
-    components: { CatalogSidebarMenu, CatalogSidebarFilter },
+    components: { CatalogSidebarMenu },
   }
 </script>
 
