194caa94f41aee68a1de878220c27d6b31165d33
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2022-08-25 14:40:16 +0300

    fix: changed env variables

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -4,8 +4,8 @@ import pwa from './config/pwa'
 export default {
   // Указываем порт, на котором будет работать приложение.
   server: {
-    port: process.env.PORT,
-    host: 'localhost',
+    host: process.env.APP_HOST || '0.0.0.0',
+    port: process.env.APP_PORT || 3000,
   },
 
   // Передаём на фронт данные из .env. Эти переменные будет доступны через process.env.[...].
