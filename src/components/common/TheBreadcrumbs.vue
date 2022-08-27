b736ce7b465f36036c092b27dc6e4045706efbb3
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2022-08-27 17:43:58 +0300

    feat: changed breadcrumbs. error. layouts and etc

---
+++ src/components/common/TheBreadcrumbs.vue
@@ -0,0 +1,33 @@
+<!-- <template>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul class="breadcrumb-list">
+      <li class="breadcrumb-link">Главная</li>
+      <li class="breadcrumb-link" aria-current="location">Банки</li>
+    </ul>
+  </nav>
+</template> -->
+
+<template>
+  <nav class="breadcrumb" aria-label="breadcrumbs">
+    <ul class="breadcrumb-list">
+      <li
+        v-for="(link, index) in breadcrumbs"
+        :key="index"
+        class="breadcrumb-link"
+      >
+        <NuxtLink :to="link.path">{{ link.name }} </NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    name: 'TheBreadcrumbs',
+    computed: {
+      ...mapState('breadcrumbs', ['breadcrumbs']),
+    },
+  }
+</script>
