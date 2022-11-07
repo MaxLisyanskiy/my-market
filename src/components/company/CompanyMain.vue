2cc7e3233396974e5f90d89014e4f7c2d773ece9
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2022-11-07 12:02:16 +0300

    fix: corrected pagination style & removed imgs

--- src/components/company/CompanyMain.vue
+++ src/components/company/CompanyMain.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="Main" class="company content-row">
     <div class="wrapper">
-      <img src="@/assets/img/ТК 2.png" alt="" class="comapny-main__img" />
+      <img src="@/assets/img/img-not_found.jpg" alt="" class="comapny-main__img" />
     </div>
   </section>
 </template>
--- src/components/home/HomeProducts.vue
+++ src/components/home/HomeProducts.vue
@@ -11,7 +11,7 @@
           <span class="products-block__text">Графин 1.75 л. Январский с крышкой</span>
         </div>
         <img
-          src="@/assets/img/Графин 1,75л _Январский_ с крышкой 1.png"
+          src="@/assets/img/img-not_found.jpg"
           alt="Графин 1.75 л. Январский с крышкой"
           class="products-block__img"
         />
