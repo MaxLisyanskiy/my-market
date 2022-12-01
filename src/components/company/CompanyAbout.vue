6945bbbb36e53a13405366cd0a333c76c9ad9fae
Robert <rickmozhet@gmail.com>
2022-12-01 17:12:30 +0300

    change/companyAboutPage

--- src/components/company/CompanyAbout.vue
+++ src/components/company/CompanyAbout.vue
@@ -20,14 +20,17 @@
         </ul>
       </div>
 
-      <div class="company-description">
-        <div :class="{ show: showCompanyDescription }" class="company-description__block" ref="description">
-          <span class="company-description__title">Описание</span>
-          <div class="company-description__flex" v-if="isCompanyEdit">
+      <div
+        :class="{ showDescription: !companyDescriptionEditor || !companyRequisitesEditor }"
+        class="company-description"
+      >
+        <div :class="{ show: !companyDescriptionEditor }" class="company-description__block">
+          <span class="company-description__title">{{ descriptionTitle }}</span>
+          <div class="company-description__flex">
             <div
-              v-if="redactCompanyDescription"
-              @click="() => ((showCompanyBlock = true), (showCompanyDescription = true))"
+              v-if="companyDescriptionEditor"
               class="company-edit"
+              @click="() => ((companyDescriptionEditor = !companyDescriptionEditor), (companyShowPlate = true))"
             >
               <div class="company-edit__icon">
                 <CompanyEditSvg class="company-edit__icon-img" />
@@ -35,37 +38,69 @@
               <span class="company-edit__text">Редактировать</span>
             </div>
             <div
-              v-if="saveCompanyDescription"
-              class="company-save"
-              @click="() => ((showCompanyBlock = false), (showCompanyDescription = false))"
+              v-if="!companyDescriptionEditor"
+              class="company-save desktop"
+              @click="() => ((companyDescriptionEditor = true), save(descriptionTitle))"
             >
               <span class="company-save__text">Сохранить</span>
             </div>
 
             <div
-              v-if="saveCompanyDescription"
-              class="company-cancel"
-              @click="() => ((showCompanyBlock = false), (showCompanyDescription = false))"
+              v-if="!companyDescriptionEditor"
+              class="company-cancel desktop"
+              @click="() => ((companyDescriptionEditor = true), (companyShowPlate = false))"
             >
               <span>x</span>
             </div>
           </div>
 
-          <AppSwiper v-if="company.images.length > 1" :swiper-config="swiperConfig" :images="company.images" />
-
           <!-- eslint-disable-next-line vue/no-v-html -->
-          <div class="company-description__text" v-html="company.description"></div>
-          <span class="company-description__read">Показать полностью</span>
+          <div v-if="companyDescriptionEditor" class="company-description__text" v-html="companyInfo.description"></div>
+          <span v-if="companyDescriptionEditor" class="company-description__read">Показать полностью</span>
+
+          <div v-if="!companyDescriptionEditor" class="company-redact__slider">
+            <div class="company-redact__slider-block">
+              <img
+                class="company-redact__slider-img"
+                src="https://s13.stc.yc.kpcdn.net/share/i/instagram/B44solahwlo/wr-1280.webp"
+                alt=""
+              />
+              <CompanyDeleteSvg class="company-redact__slider-delete" />
+            </div>
+            <div class="company-add__slider">
+              <label for="companySliderAdd">
+                <CompanyCameraSvg class="company-add__slider-img" />
+                <span class="company-add__slider-text">Добавить фото</span>
+              </label>
+              <input id="companySliderAdd" class="company-add__slider-input" type="file" />
+            </div>
+          </div>
+
+          <div v-if="!companyDescriptionEditor" class="description-redact">
+            <textarea class="description-redact__textarea" :placeholder="companyInfo.description"></textarea>
+          </div>
+
+          <div v-if="!companyDescriptionEditor" class="company-save mob">
+            <span class="company-save__text" @click="() => ((companyDescriptionEditor = true), save(descriptionTitle))"
+              >Сохранить</span
+            >
+          </div>
+
+          <div v-if="!companyDescriptionEditor" class="company-cancel mob">
+            <span class="company-cancel" @click="() => ((companyDescriptionEditor = true), (companyShowPlate = false))"
+              >X</span
+            >
+          </div>
         </div>
 
-        <div ref="requisites" class="company-requisites" :class="{ show: showCompanyRequisites }">
-          <div class="company-requisites__block">
-            <h3 class="company-requisites__title">Реквизиты</h3>
-            <div class="company-requisites__flex" v-if="isCompanyEdit">
+        <div ref="requisites" class="company-requisites" :class="{ show: !companyRequisitesEditor }">
+          <div class="company-requisites__block company-requisites__block-redact">
+            <h3 class="company-requisites__title">{{ requisitesTitle }}</h3>
+            <div class="company-requisites__flex">
               <div
-                v-if="redactCompanyRequisites"
-                @click="() => ((showCompanyBlock = true), (showCompanyRequisites = true))"
+                v-if="companyRequisitesEditor"
                 class="company-edit"
+                @click="() => ((companyRequisitesEditor = !companyRequisitesEditor), (companyShowPlate = true))"
               >
                 <div class="company-edit__icon">
                   <CompanyEditSvg class="company-edit__icon-img" />
@@ -73,24 +108,101 @@
                 <span class="company-edit__text">Редактировать</span>
               </div>
               <div
-                v-if="saveCompanyRequisites"
-                class="company-save"
-                @click="() => ((showCompanyBlock = false), (showCompanyRequisites = false))"
+                v-if="!companyRequisitesEditor"
+                class="company-save desktop"
+                @click="() => ((companyRequisitesEditor = true), save(requisitesTitle))"
               >
                 <span class="company-save__text">Сохранить</span>
               </div>
 
               <div
-                v-if="saveCompanyRequisites"
-                class="company-cancel"
-                @click="() => ((showCompanyBlock = false), (showCompanyRequisites = false))"
+                v-if="!companyRequisitesEditor"
+                class="company-cancel desktop"
+                @click="() => ((companyRequisitesEditor = true), (companyShowPlate = false))"
               >
                 <span>x</span>
               </div>
             </div>
+
+            <AppSwiper v-if="company.images.length > 1" :swiper-config="swiperConfig" :images="company.images" />
+
+            <div v-if="!companyRequisitesEditor" class="table table-redact">
+              <div class="table-block">
+                <div class="table-block__left">Название компании:</div>
+                <div class="table-block__right table-redact__right">
+                  <input class="table-redact__input" type="text" :placeholder="companyInfo.company_name" />
+                </div>
+              </div>
+              <div class="table-block">
+                <div class="table-block__left">Юридический адрес:</div>
+                <div class="table-block__right table-redact__right">
+                  <input class="table-redact__input" type="text" :placeholder="companyInfo.legal_address" />
+                </div>
+              </div>
+              <div class="table-block">
+                <div class="table-block__left">Фактический адрес:</div>
+                <div class="table-block__right table-redact__right">
+                  <input class="table-redact__input" type="text" :placeholder="companyInfo.actual_address" />
+                </div>
+              </div>
+              <div class="table-block">
+                <div class="table-block__left">ИНН:</div>
+                <div class="table-block__right table-redact__right">
+                  <input
+                    v-model="companyInfo.inn"
+                    class="table-redact__input"
+                    type="text"
+                    :placeholder="companyInfo.inn"
+                  />
+                </div>
+              </div>
+              <div class="table-block">
+                <div class="table-block__left">ОГРН:</div>
+                <div class="table-block__right table-redact__right">
+                  <input class="table-redact__input" type="text" :placeholder="companyInfo.ogrn" />
+                </div>
+              </div>
+              <div class="table-block">
+                <div class="table-block__left">КПП</div>
+                <div class="table-block__right table-redact__right">
+                  <input class="table-redact__input" type="text" :placeholder="companyInfo.kpp" />
+                </div>
+              </div>
+              <div class="table-block">
+                <div class="table-block__left">Генеральный директор:</div>
+                <div class="table-block__right table-redact__right">
+                  <input class="table-redact__input" type="text" :placeholder="companyInfo.ceo" />
+                </div>
+              </div>
+              <div class="table-block">
+                <div class="table-block__left">Директор:</div>
+                <div class="table-block__right table-redact__right">
+                  <input class="table-redact__input" type="text" :placeholder="companyInfo.director" />
+                </div>
+              </div>
+
+              <div v-if="!companyRequisitesEditor" class="company-save mob">
+                <span
+                  class="company-save__text"
+                  @click="() => ((companyRequisitesEditor = true), save(requisitesTitle))"
+                  >Сохранить</span
+                >
+              </div>
+
+              <div v-if="!companyRequisitesEditor" class="company-cancel mob">
+                <span
+                  class="company-cancel"
+                  @click="() => ((companyRequisitesEditor = true), (companyShowPlate = false))"
+                  >X</span
+                >
+              </div>
+            </div>
           </div>
 
-          <div v-if="company.requisites || company.inn" class="table">
+          <div
+            v-if="(company.requisites && companyRequisitesEditor) || (company.inn && companyRequisitesEditor)"
+            class="table"
+          >
             <div v-if="company.requisites?.company_name" class="table-block">
               <div class="table-block__left">Название компании:</div>
               <div class="table-block__right">{{ company.requisites.company_name }}</div>
@@ -139,10 +251,12 @@
 
   import AppSwiper from '~/components/UI/AppSwiper.vue'
   import CompanyEditSvg from '@/assets/img/icons/svg/edit.svg?inline'
+  import CompanyDeleteSvg from '@/assets/img/icons/svg/close-share.svg?inline'
+  import CompanyCameraSvg from '@/assets/img/icons/svg/camera.svg?inline'
 
   export default {
     name: 'CompanyAbout',
-    components: { AppSwiper, CompanyEditSvg },
+    components: { AppSwiper, CompanyEditSvg, CompanyCameraSvg, CompanyDeleteSvg },
     props: {
       company: {
         type: Object,
@@ -159,44 +273,21 @@
     },
     data() {
       return {
+        requisitesTitle: 'Реквизиты',
+        descriptionTitle: 'Описание',
         activeLink: 'description',
         links: [
           { name: 'Описание', goTo: 'description' },
           { name: 'Реквизиты', goTo: 'requisites' },
         ],
+        companyDescriptionEditor: true,
+        companyRequisitesEditor: true,
+        companyShowPlate: false,
 
-        // Пример переменной для сокращения
-        // showCompanyDescriptionEditor: false,
-
-        // Сократить
-        redactCompanyDescription: true,
-        showCompanyDescription: false,
-        hideCompanyDescription: false,
-        saveCompanyDescription: false,
-        redactCompanyRequisites: true,
-        showCompanyRequisites: false,
-        hideCompanyRequisites: false,
-        saveCompanyRequisites: false,
-        showCompanyBlock: false,
-        hideCompanyBlock: false,
-
-        // Пример объекта для редактирования
-        companyInfo: {},
+        companyInfo: {
+        },
       }
     },
-    mounted() {
-      // Пример для добавления значений в объект в data
-      this.companyInfo = {
-        // description,
-        company_name: this.company.company_name ?? '',
-        inn: this.company.inn ?? '',
-        // ...
-      }
-
-      // console.log(this.company)
-    },
-
-    // Переместить
     computed: {
       ...mapState('company', ['companySearchInput', 'companySearchQuery']),
       ...mapState('global', ['firstPageVisit']),
@@ -208,50 +299,35 @@
         return false
       },
     },
-
-    // Переместить
     watch: {
-      showCompanyBlock() {
-        if (this.showCompanyBlock === true) {
-          this.hideCompanyBlock = false
+      companyShowPlate() {
+        if (this.companyShowPlate === true) {
           document.querySelector('body').style.overflowY = 'hidden'
           document.querySelector('.backgroundPlateTwo').classList.add('active')
-          if (this.showCompanyDescription === true) {
-            this.saveCompanyDescription = true
-            this.redactCompanyDescription = false
-          } else if (this.showCompanyRequisites === true) {
-            this.saveCompanyRequisites = true
-            this.redactCompanyRequisites = false
-          }
         } else {
           document.querySelector('body').removeAttribute('style')
           document.querySelector('.backgroundPlateTwo').classList.remove('active')
-          if (this.showCompanyDescription === false) {
-            this.saveCompanyDescription = false
-            this.redactCompanyDescription = true
-            this.saveCompanyRequisites = false
-            this.redactCompanyRequisites = true
-          }
         }
       },
     },
-
-    // Переместить
-    hideCompanyBlock() {
-      if (this.hideCompanyBlock === true) {
-        this.showCompanyBlock = false
-        this.showCompanyDescription = false
-        this.showCompanyRequisites = false
-        if (this.showCompanyDescription === false) {
-          alert('HideDescription')
-        }
-      } else {
-        this.hideCompanyBlock = false
-        this.showCompanyBlock = true
+    mounted() {
+      this.companyInfo = {
+        description: this.company.description ?? '',
+        company_name: this.company.company_name ?? '',
+        legal_address: this.company.legal_address ?? '',
+        actual_address: this.company.actual_address ?? '',
+        inn: this.company.inn ?? '',
+        ogrn: this.company.ogrn ?? '',
+        kpp: this.company.kpp ?? '',
+        ceo: this.company.ceo ?? '',
+        director: this.company.director ?? '',
       }
     },
-
     methods: {
+      save(name) {
+        alert(name)
+        this.companyShowPlate = false
+      },
       scrollTo(refName) {
         const element = this.$refs[refName]
         const top = element.offsetTop
