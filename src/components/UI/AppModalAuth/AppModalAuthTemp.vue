8f8851c18184eb8ad8b2e2cbdbecf78d4b0ea7a1
Robert <rickmozhet@gmail.com>
2022-12-28 08:10:59 +0300

    change/temporaryRegistration

---
+++ src/components/UI/AppModalAuth/AppModalAuthTemp.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    class="modal-dialog auth__wrapp signup"
+    role="dialog"
+    aria-labelledby="modalHeader"
+    aria-describedby="modalDesc"
+    @click.stop
+  >
+    <div class="auth-form signup__form">
+      <h3 id="modalHeader" class="signup__title signup__title-temp">Регистрация компании</h3>
+      <span class="auth-form__text">По вопросам регистрации напишите, пожалуйста, в службу поддержки:</span>
+      <span class="auth-form__email" @click="handleCopyURL">{{ email }}</span>
+      <p class="auth-form__link" @click.stop="SET_WINDOW_TO_SHOW('login')">Назад</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapMutations } from 'vuex'
+
+  export default {
+    name: 'AppModalAuthRegister',
+    data() {
+      return {
+        email: 'support@vale.su',
+      }
+    },
+    methods: {
+      handleCopyURL() {
+        navigator.clipboard.writeText(this.email)
+        this.$notify({
+          title: '',
+          message: 'Почта успешно скопирована',
+          type: 'success',
+        })
+      },
+      ...mapMutations('modal-auth', ['SET_WINDOW_TO_SHOW']),
+    },
+  }
+</script>
---
+++ src/components/auth/AuthSignUpTemp.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="auth">
+    <div class="auth-form signup signup__form">
+      <h3 id="modalHeader" class="signup__title signup__title-temp">Регистрация компании</h3>
+      <span class="auth-form__text">По вопросам регистрации напишите, пожалуйста, в службу поддержки:</span>
+      <span class="auth-form__email" @click="handleCopyURL">{{ email }}</span>
+      <nuxt-link to="/login">
+        <p class="auth-form__link">Назад</p>
+      </nuxt-link>
+    </div>
+  </section>
+</template>
+
+<script>
+  import { mapMutations } from 'vuex'
+
+  export default {
+    name: 'AuthSignup',
+    data() {
+      return {
+        email: 'support@vale.su',
+      }
+    },
+    methods: {
+      handleCopyURL() {
+        navigator.clipboard.writeText(this.email)
+        this.$notify({
+          title: '',
+          message: 'Почта успешно скопирована',
+          type: 'success',
+        })
+      },
+      ...mapMutations('modal-auth', ['SET_WINDOW_TO_SHOW']),
+    },
+  }
+</script>
+<style lang="scss" scoped>
+  .signup__title-temp {
+    line-height: 0;
+  }
+  .auth-form {
+    background: none;
+    box-shadow: none;
+    &__email,
+    &__text {
+      color: #808080;
+      font-weight: 400;
+      font-size: 16px;
+      line-height: 19px;
+      text-align: center;
+    }
+    &__email {
+      color: #404040;
+      cursor: pointer;
+    }
+    &__link {
+      font-size: 12px;
+      line-height: 14px;
+    }
+  }
+</style>
